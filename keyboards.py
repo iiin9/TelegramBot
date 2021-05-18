02894a970ddda052b4b5f5987fabc7828406c47e
iiin9 <51161222+iiin9@users.noreply.github.com>
2021-05-18 13:43:00 +0300

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import (InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup)
-
-btn_check_1 = KeyboardButton("Проверить Мерседес")
-btn_check_2 = KeyboardButton("В007АН777")
-greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(btn_check_1).add(btn_check_2)
-inline_btn_1 = InlineKeyboardButton("База2", callback_data="Base2")
-inline_kb = InlineKeyboardMarkup().add(inline_btn_1)
